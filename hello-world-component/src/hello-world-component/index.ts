6c00e7e887765a2306e3c796ddc322517270955a
Brian Mikinski <brian.mikinski@gmail.com>
2020-04-10 09:43:04 -0500

    add schema file

--- hello-world-component/src/hello-world-component/index.ts
+++ hello-world-component/src/hello-world-component/index.ts
@@ -1,5 +1,16 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
+// tools provided by angluar dev kit you might want to use
+// import {
+//   JsonAstObject,
+//   JsonObject,
+//   JsonValue,
+//   Path,
+//   normalize,
+//   parseJsonAst,
+//   strings,
+// } from '@angular-devkit/core';
+
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
