17ab69d87001549ac21cc1ac0f1e9d7e5eb11bfc
Brian Mikinski <brian.mikinski@gmail.com>
2020-04-10 15:48:19 -0500

    update schematic

--- hello-world-component/src/hello-world-component/index.ts
+++ hello-world-component/src/hello-world-component/index.ts
@@ -1,4 +1,19 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  apply,
+  url,
+  move,
+  forEach,
+  FileEntry,
+  mergeWith,
+  MergeStrategy,
+  template
+} from '@angular-devkit/schematics';
+import { join, normalize } from 'path';
+
+import { getWorkspace } from '@schematics/angular/utility/config';
 
 // tools provided by angluar dev kit you might want to use
 // import {
@@ -11,11 +26,55 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 //   strings,
 // } from '@angular-devkit/core';
 
+export function setupOptions(host: Tree, options: any): Tree {
+  const workspace = getWorkspace(host);
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
+  if (!options.project) {
+    options.project = Object.keys(workspace.projects)[0];
+  }
+
+  const project = workspace.projects[options.project];
+  options.path = join(normalize(project.root), 'src');
+
+  return host;
+}
+
+/**
+ * Combining two different tutorials here from Matt Raible and Tomas Trajan -
+ * 
+ * You don't have to export the function as default. You can also have more than one rule factory
+ * per file.
+ * @param _options 
+ * 
+ */
 export function helloWorldComponent(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+
+    setupOptions(tree, _options);
+
+    const movePath = normalize(_options.path + '/');
+
+    // array is an array of rules
+    const templateSource = apply(url('./files/src'), [
+      template({ ..._options }),
+      move(movePath),
+      // fix for https://github.com/angular/angular-cli/issues/11337
+      forEach((fileEntry: FileEntry) => {
+
+        if (tree.exists(fileEntry.path)) {
+          tree.overwrite(fileEntry.path, fileEntry.content);
+        }
+
+        return fileEntry;
+      }),
+
+    ]);
+
+    const rule = mergeWith(templateSource, MergeStrategy.Overwrite);
+
+    tree.create('hello.ts', 'console.log("Hello, World")');
+    //return tree;
+
+    return rule(tree, _context);
   };
 }
--- hello-world-component/src/hello-world-component/index_spec.ts
+++ hello-world-component/src/hello-world-component/index_spec.ts
@@ -1,16 +1,62 @@
 import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
-
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
-
 describe('hello-world-component', () => {
+
+  // Tomas Trajan tutorial
   it('works', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('hello-world-component', {}, Tree.empty());
+    runner.runSchematicAsync('hello-world-component', {}, Tree.empty()).toPromise().then(tree => {
 
-    expect(tree.files).toEqual([]);
+      expect(tree.files).toEqual(['/hello.ts']);
+    });
+    
   });
+
+  // Matt Raible tutorial
+  
+  const schematicRunner = new SchematicTestRunner(
+    'schematics',
+    path.join(__dirname, './../collection.json'),
+  );
+
+  const workspaceOptions: any = { 
+    name: 'workspace',
+    newProjectRoot: 'projects',
+    version: '0.5.0',
+  };
+
+  const appOptions: any = { 
+    name: 'schematest'
+  };
+
+  const schemaOptions: any = { 
+    name: 'foo'
+  };
+
+  let appTree: UnitTestTree;
+
+  beforeEach(() => { 
+    //appTree = 
+    schematicRunner.runExternalSchematicAsync('@schematics/angular', 'workspace', workspaceOptions)
+    .toPromise().then( tree => {
+      appTree = tree;
+    });
+    schematicRunner.runExternalSchematicAsync('@schematics/angular', 'application', appOptions, appTree)
+    .toPromise().then( tree => {
+      appTree = tree;
+    });;
+  });
+
+  it('works', () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    runner.runSchematicAsync('hello-world-component', schemaOptions, appTree).toPromise().then(tree => {
+      const appComponent = tree.readContent('/projects/schematest/src/app/app.component.ts'); 
+      expect(appComponent).toContain(`name = '${schemaOptions.name}'`); 
+    });
+  });
+
 });
