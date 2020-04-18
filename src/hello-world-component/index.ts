e7390bf20f6d50b7916a2eca0be91a479b3a191f
Brian Mikinski <brian.mikinski@gmail.com>
2020-04-18 10:30:37 -0500

    Fix schematic debug

--- src/hello-world-component/index.ts
+++ src/hello-world-component/index.ts
@@ -9,10 +9,16 @@ import {
   FileEntry,
   mergeWith,
   MergeStrategy,
-  template
+  template,
+  //applyTemplates,
 } from '@angular-devkit/schematics';
 import { join, normalize } from 'path';
 import { getWorkspace } from '@schematics/angular/utility/config';
+import { HelloWorldSchema } from './schema';
+import { classify, dasherize, camelize, underscore } from '@angular-devkit/core/src/utils/strings';
+
+// required for template manipulations
+const stringUtils = {classify, dasherize, camelize, underscore };
 
 // tools provided by angluar dev kit you might want to use
 // import {
@@ -25,13 +31,14 @@ import { getWorkspace } from '@schematics/angular/utility/config';
 //   strings,
 // } from '@angular-devkit/core';
 
-export function setupOptions(host: Tree, options: any): Tree {
+export function setupOptions(host: Tree, options: HelloWorldSchema): Tree {
+  
   const workspace = getWorkspace(host);
   if (!options.project) {
     options.project = Object.keys(workspace.projects)[0];
   }
-  const project = workspace.projects[options.project];
 
+  const project = workspace.projects[options.project];
   options.path = join(normalize(project.root), 'src');
   return host;
 }
@@ -44,16 +51,35 @@ export function setupOptions(host: Tree, options: any): Tree {
  * @param _options 
  * 
  */
-export function helloWorldComponent(_options: any): Rule {
+export function helloWorldComponent(_options: HelloWorldSchema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
 
     setupOptions(tree, _options);
 
     const movePath = normalize(_options.path + '/');
 
+
+    // must define and apply classify, dasherize, etc here to manipulate strings
+    // and templates
+
     // array is an array of rules
     const templateSource = apply(url('./files/src'), [
-      template({ ..._options }),
+      
+      // still trying to figure out the difference between
+      // template and applyTemplates?
+      template({
+        ..._options,
+        classify: stringUtils.classify,
+        dasherize: stringUtils.dasherize,
+        name: _options.name
+      }),
+      
+      // applyTemplates({
+      //   ..._options,
+      //   classify: stringUtils.classify,
+      //   dasherize: stringUtils.dasherize,
+      //   name: _options.name
+      // }),
       move(movePath),
       // fix for https://github.com/angular/angular-cli/issues/11337
       forEach((fileEntry: FileEntry) => {
@@ -70,7 +96,7 @@ export function helloWorldComponent(_options: any): Rule {
 
     const rule = mergeWith(templateSource, MergeStrategy.Overwrite);
 
-    //tree.create('hello.ts', 'console.log("Hello, World")');
+    tree.create('hello.ts', 'console.log("Hello, World")');
     //return tree;
 
     return rule(tree, _context);
---
+++ src/hello-world-component/schema.ts
@@ -0,0 +1,10 @@
+export interface HelloWorldSchema {
+    // the name of the service.
+    name: string;
+  
+    // the path to create the service.
+    path?: string;
+  
+    // the name of the project.
+    project?: string;
+  }
